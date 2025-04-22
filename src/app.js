1e38381bca4d378c6f49aa3d17dbe9d53ae8af52
Gunjan Verma <vermagunjan9927@gmail.com>
2025-04-22 17:04:28 +0530

    create signup post api

--- src/app.js
+++ src/app.js
@@ -1,6 +1,20 @@
 const express = require("express");
 const connectDB = require("../config/database");
 const app = express();
+const User = require("./model/user")
+
+// handle and process the json data, reads the json data and convert it to js object
+app.use(express.json())
+
+app.post("/signup",async (req,res)=>{
+    const user = new User(req.body);
+    try{
+        await user.save();
+        res.send("user added successfully");
+    } catch(err){
+        res.status(400).send("error saving the user",err);
+    }
+});
 
 connectDB().then(()=>{
     console.log("database is connected");
