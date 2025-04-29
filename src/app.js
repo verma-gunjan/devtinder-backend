b90e04e45afde0a171a7ad2347ff7af60a66d818
Gunjan Verma <vermagunjan9927@gmail.com>
2025-04-29 14:18:55 +0530

    add validation on database level

--- src/app.js
+++ src/app.js
@@ -6,13 +6,71 @@ const User = require("./model/user")
 // handle and process the json data, reads the json data and convert it to js object
 app.use(express.json())
 
-app.post("/signup",async (req,res)=>{
-    const user = new User(req.body);
-    try{
+app.post("/signup", async (req, res) => {
+    try {
+        // First, check if email already exists
+        const emailExists = await User.findOne({ emailId: req.body.emailId });
+        if (emailExists) {
+            return res.status(400).send('Email already exists');
+        }
+
+        // If not, create and save new user
+        const user = new User(req.body);
         await user.save();
-        res.send("user added successfully");
+        res.send("User added successfully");
+    } catch (err) {
+        res.status(400).send("Error saving the user: " + err.message);
+    }
+});
+
+app.get("/user", async (req, res)=>{
+    const userEmail = req.body.emailId;
+    try{
+        const user = await User.find({emailId: userEmail});
+        if(!user){
+            res.status(404).send("user not found");
+        }else {
+            res.send(user); 
+        }
     } catch(err){
-        res.status(400).send("error saving the user",err);
+        res.status(400).send("something went wrong");
+    }
+});
+
+app.delete("/user", async (req, res)=>{
+    const userId = req.body.userId;
+    try{
+        const user = await User.findByIdAndDelete(userId);
+        res.send("user deleted successfully");
+    } catch(err){
+        res.status(400).send("something went wrong");
+    }
+})
+
+app.patch("/user",async (req, res)=>{
+    const userId = req.body.userId;
+    const data = req.body;
+    try{
+        const user = await User.findByIdAndUpdate({_id: userId}, data, {
+            returnDocument: "after",
+            runValidators: true,
+        });
+        if (!user) {
+            return res.status(404).send("User not found");
+        }
+        res.send("user updated successfully");
+    }catch(err){
+        res.status(400).send("something went wrong" + err.message);
+    }
+});
+
+app.get("/feed", async (req, res)=>{
+    try{
+        // passing empty obj will give all users list
+        const users = await User.find({});
+        res.send(users);
+    } catch(err){
+        res.status(400).send("something went wrong");
     }
 });
 
--- src/model/user.js
+++ src/model/user.js
@@ -2,20 +2,53 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     firstName: {
-        type: String
+        type: String,
+        required: true,
+        minLength: 4,
+        maxLength: 30,
     },
     lastName: {
-        type: String
+        type: String,
     },
     emailId: {
-        type: String
+        type: String,
+        lowercase: true,
+        required: true,
+        unique: true,
+        trim: true,
+    },
+    password:{
+        type: String,
+        required: true,
     },
     age: {
-        type: String
+        type: Number,
+        min: 18,
     },
     gender: {
-        type: String
+        type: String,
+        required: true,
+        validate(value){
+           if(!["male","female","others"].includes(value)){
+                throw new Error("Gender data is not valid");
+           }
+        },
+    },
+    profilePictureUrl: {
+        type: String,
+    },
+    additionalPictures: {
+        type: [String],
+    },
+    about: {
+        type: String,
+        default: "This is a default description of user",
+    },
+    skills: {
+        type: [String],
     }
+},{
+    timestamps: true,
 });
 
 module.exports = mongoose.model("User", userSchema);
