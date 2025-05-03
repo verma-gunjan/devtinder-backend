c5981aab01e9747720a001668295495b22a9c222
Gunjan Verma <vermagunjan9927@gmail.com>
2025-05-03 07:46:17 +0530

    create profile api

--- src/app.js
+++ src/app.js
@@ -4,9 +4,13 @@ const app = express();
 const User = require("./model/user")
 const {validateSignupData} = require("./utils/validation");
 const bcrypt = require('bcrypt');
+// const session = require('express-session');
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
 
 // handle and process the json data, reads the json data and convert it to js object
 app.use(express.json())
+app.use(cookieParser());
 
 app.post("/signup", async (req, res) => {
     try {
@@ -39,19 +43,46 @@ app.post("/login", async (req,res) =>{
         const {emailId, password} = req.body;
         const user = await User.findOne({emailId: emailId});
         if(!user){
-            throw new Error("User is not registerd");
+            throw new Error("Invalid credentials");
         }
         const isPasswordValid = await bcrypt.compare(password, user.password);
         if(isPasswordValid){
+            // creating json web token
+            const token = await jwt.sign({_id: user._id}, "DevTinder$123");
+            // sending the token to the browser
+            res.cookie("token", token);
+            console.log(token);
             res.send("Login Successfully");
         }else{
-            res.status(401).send("Incorrect password");
+            res.status(401).send("Invalid credentials");
         }
 
     } catch(err){
         res.status(400).send("Error:" + err.message);
     }
 });
+
+app.get("/profile", async (req, res) =>{
+    try{
+        // fetching token from cookies
+        const cookies = req.cookies;
+        const {token} = cookies;
+        if(!token){
+           res.status(401).send("Not logged in");
+        }
+        // decoding the token
+        const decode = jwt.verify(token, "DevTinder$123");
+        const userId = decode._id;
+        const user = await User.findById(userId);
+        if(!user){
+            res.status(404).send("user not found");
+        }else{
+            res.send(user);
+        }
+    }catch(err){
+        res.status(404).send("Error:" + err);
+    }
+})
 
 app.get("/user", async (req, res)=>{
     const userEmail = req.body.emailId;
